8f2cf34fe190d110438b07d00bf3eb65e63198e3
ssandaya <ssandaya@yahoo.com>
2016-10-07 00:55:37 -0400

    initial commit

---
+++ src/main/groovy/ConfigParser.groovy
@@ -0,0 +1,14 @@
+/**
+ * Created by ssa on 10/6/16.
+ */
+class ConfigParser {
+    public CsvTransformer.Config parse( File config ) {
+        createConfigConstructor()
+        Yaml yaml = new Yaml( new Constructor( CsvTransformer.Config.class ))
+        try {
+            return (CsvTransformer.Config)yaml.load( new FileInputStream( config ))
+        } catch ( Exception e ) {
+            e.printStackTrace()
+        }
+    }
+}
---
+++ src/main/groovy/eg/Eg.groovy
@@ -0,0 +1,13 @@
+package eg
+
+/**
+ * Created by ssa on 10/7/16.
+ */
+class Eg {
+    static void main(args){
+        def a = 'John Doe'
+        a = new Object()
+        a = 10
+        println a
+    }
+}
